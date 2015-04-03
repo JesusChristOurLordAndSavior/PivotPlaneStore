c0ac7d7d5932ddef57a9a8bedccc4568b689dca3
Luigi Mangione <louismangione@louiss-mbp.hsd1.md.comcast.net>
2015-04-03 18:16:38 -0400

    Initial Commit

--- StoreScene.m
+++ StoreScene.m
@@ -26,8 +26,34 @@
 }
 
 - (void)createSceneContents{
-    self.backgroundColor = [SKColor blueColor];
+    self.backgroundColor = [SKColor whiteColor];
+    
+    SKSpriteNode *leftButton = [SKSpriteNode spriteNodeWithImageNamed:@"LeftButton"];
+    leftButton.position = CGPointMake(50, 60);
+    leftButton.name = @"LeftButton";
+    [self addChild:leftButton];
+    
+    SKSpriteNode *rightButton = [SKSpriteNode spriteNodeWithImageNamed:@"RightButton"];
+    rightButton.position = CGPointMake(self.frame.size.width - 50, 60);
+    rightButton.name = @"RightButton";
+    [self addChild:rightButton];
+    
+    SKSpriteNode *selectButton = [SKSpriteNode spriteNodeWithImageNamed:@"SelectButton"];
+    selectButton.position = CGPointMake(self.frame.size.width / 2.0, 60);
+    selectButton.name = @"SelectButton";
+    [self addChild:selectButton];
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKNode *node = [self nodeAtPoint:location];
+    
+    if ([node.name isEqualToString:@"LeftButton"]){
+        //DO SOMETHING
+        NSLog(@"Touched Left");
+        
+    }
+}
 
 @end
