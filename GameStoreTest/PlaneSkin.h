ce0673d710789a6c8a95b8e5a7defc8ffa5e65b9
AppRoar Studios <AppRoar@Luigis-MBP-3.hsd1.md.comcast.net>
2015-04-03 18:39:55 -0400

    Initial COOmitT

---
+++ GameStoreTest/PlaneSkin.h
@@ -0,0 +1,13 @@
+//
+//  PlaneSkin.h
+//  GameStoreTest
+//
+//  Created by AppRoar Studios on 4/3/15.
+//  Copyright (c) 2015 AppRoar Studios. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface PlaneSkin : SKSpriteNode
+
+@end
---
+++ GameStoreTest/PlaneSkin.m
@@ -0,0 +1,14 @@
+//
+//  PlaneSkin.m
+//  GameStoreTest
+//
+//  Created by AppRoar Studios on 4/3/15.
+//  Copyright (c) 2015 AppRoar Studios. All rights reserved.
+//
+
+#import "PlaneSkin.h"
+
+@implementation PlaneSkin
+
+@end
+
